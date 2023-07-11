c65b8aa0dfa14b7ac84295a1c0afe72bdda68b6d
lzzhy <liting513@aliyun.com>
2023-07-11 16:24:47 +0800

    feat: 添加搜索2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,9 @@
+export declare const searchFile: (words: string) => Promise<any>;
 export declare const listFiles: (type: string, folderId?: string) => Promise<any>;
 export default class googleDrive {
     static _instance: googleDrive;
     static getInstance(): googleDrive;
     static auth(type: string, folderId?: string): Promise<any>;
     static listFiles(type: string, folderId?: string): Promise<any>;
+    static searchFiles(words: string): Promise<any>;
 }
